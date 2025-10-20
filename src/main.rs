0de74c063b7b9d87fda58920ff57c3cb31f77173
nishant <nishun2005@gmail.com>
2025-10-20 14:25:03 -0700

    parse for function semantics and add tests

--- src/main.rs
+++ src/main.rs
@@ -17,10 +17,49 @@ enum Token {
 
     #[regex(r"[0-9]+", |lex| lex.slice().parse::<i64>().map_err(|_| ()))]
     Number(i64),
+
+    #[regex(r"[a-zA-Z_][a-zA-Z0-9_]*", |lex| lex.slice().to_string())]
+    Identifier(String),
+
+    #[token("def")]
+    Def,
+
+    #[token("(")]
+    LeftParenthesis,
+
+    #[token(")")]
+    RightParenthesis,
+
+    #[token(":")]
+    Colon,
+
+    #[token(",")]
+    Comma,
+    
+    #[token("for")]
+    For,
+
+    #[token("in")]
+    In,
+
+    #[token("=")]
+    Equals,
+
+    #[token("return")]
+    Return,
 }
 
 fn main() {
     for token in Token::lexer("11 + 2 * 3") {
         println!("{:?}", token);
     }
+    println!("----Test 1----");
+    for token in Token::lexer("a = 1 + 1") {
+        println!("{:?}", token);
+    }
+    println!("----Test 2----");
+    for token in Token::lexer("def add(x, y): return x + y") {
+        println!("{:?}", token);
+    }
+    println!("----Test 3----");
 }
