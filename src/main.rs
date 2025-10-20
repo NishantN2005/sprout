6a56aa7db7a1a1cb2790b27a883079184ac20d5a
nishant <nishun2005@gmail.com>
2025-10-20 14:04:19 -0700

    parse for basic arithematic ops and integers

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
-fn main() {
-    println!("Hello world!");
+use logos::Logos;
+
+#[derive(Logos, Debug, PartialEq)]
+#[logos(skip r"[ \t\n\f]+")] // Ignore this regex pattern between tokens
+enum Token {
+    #[token("+")]
+    Addition,
+
+    #[token("-")]
+    Subtraction,
+
+    #[token("*")]
+    Multiplication,
+
+    #[token("/")]
+    Division,
+
+    #[regex(r"[0-9]+", |lex| lex.slice().parse::<i64>().map_err(|_| ()))]
+    Number(i64),
 }
 
+fn main() {
+    for token in Token::lexer("11 + 2 * 3") {
+        println!("{:?}", token);
+    }
+}
