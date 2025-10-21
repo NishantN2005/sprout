d819c4deebee3e7f19fd4d363345ae609d63d230
nishant <nishun2005@gmail.com>
2025-10-21 11:58:05 -0700

    redo expression node so it works with chumsky

--- src/main.rs
+++ src/main.rs
@@ -5,61 +5,44 @@ use chumsky::prelude::*;
 #[logos(skip r"[ \t\n\f]+")] // Ignore this regex pattern between tokens
 
 enum Token {
-    #[token("+")]
-    Addition,
+    // Operators / punctuation
+    #[token("+")] Addition,
+    #[token("-")] Subtraction,
+    #[token("*")] Multiplication,
+    #[token("/")] Division,
+    #[token("(")] LeftParenthesis,
+    #[token(")")] RightParenthesis,
+    #[token(":")] Colon,
+    #[token(",")] Comma,
+    #[token("=")] Equals,
 
-    #[token("-")]
-    Subtraction,
-
-    #[token("*")]
-    Multiplication,
-
-    #[token("/")]
-    Division,
+    // Keywords 
+    #[token("def")] Def,
+    #[token("for")] For,
+    #[token("in")] In,
+    #[token("return")] Return,
 
     #[regex(r"[0-9]+", |lex| lex.slice().parse::<i64>().map_err(|_| ()))]
     Number(i64),
 
     #[regex(r"[a-zA-Z_][a-zA-Z0-9_]*", |lex| lex.slice().to_string())]
     Identifier(String),
-
-    #[token("def")]
-    Def,
-
-    #[token("(")]
-    LeftParenthesis,
-
-    #[token(")")]
-    RightParenthesis,
-
-    #[token(":")]
-    Colon,
-
-    #[token(",")]
-    Comma,
-    
-    #[token("for")]
-    For,
-
-    #[token("in")]
-    In,
-
-    #[token("=")]
-    Equals,
-
-    #[token("return")]
-    Return,
 }
 
+#[derive(Debug, Clone)]
 pub enum Expression {
     Number(i64),
     Identifier(String),
-    BinaryOperation {
-        left: Box<Expression>,
-        operator: Token,
-        right: Box<Expression>,
-    },
+    Unary { op: UnOp, rhs: Box<Expression> }, //a = 5
+    Binary { op: BinOp, lhs: Box<Expression>, rhs: Box<Expression> }, //a + b
 }
+
+#[derive(Debug, Clone, Copy)]
+pub enum BinOp { Add, Sub, Mul, Div }
+
+#[derive(Debug, Clone, Copy)]
+pub enum UnOp { Pos, Neg }
+
 
 
 fn main() {
