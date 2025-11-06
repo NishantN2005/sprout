fcb1af8e4cc8c332de964adf68b25a7993683cc5
nishant <nishun2005@gmail.com>
2025-11-06 08:58:45 -0800

    impliment function and module structures

--- src/middle/ir.rs
+++ src/middle/ir.rs
@@ -1,9 +1,20 @@
-
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct ValueId(u32);
 
+//produce functions
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Function{
+    pub name: String,
+    pub body: Vec<Inst>,
+    next_value: u32 // generate new value id's
+}
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Module{
+    pub functions: Vec<Funcunction>,
+}
 
+//defines simple instruction set
 #[derive(Debug, Clone)]
 enum Inst { 
     Const {dst: ValueId, value: i64},
@@ -17,6 +28,7 @@ enum Inst {
     Return {src: ValueId},
 }
 
+//create and getter for ValueId
 impl ValueId{
     pub fn new(id: u32) -> Self {
         ValueId(id)
@@ -24,5 +36,33 @@ impl ValueId{
 
     pub fn id(&self) -> u32 {
         self.0
+    }
+}
+
+//create and new var id generator for Function
+impl Function{
+    pub fn new(name: String) -> Self {
+        Function {
+            name,
+            body: Vec::new(),
+            next_value: 0,
+        }
+    }
+
+    pub fn fresh_value(&mut self) -> ValueId {
+        let id = ValueId(self.next_value);
+        self.next_value += 1;
+        id
+    }
+}
+
+//create and add function for Module
+impl Module{
+    pub fn new() -> Self{
+        Self { functions: Vec::new() }
+    }
+
+    pub fn add_function(&mut self, func: Function){
+        self.functions.push(func);
     }
 }
